67ffd3bd699aa99bb0cf7fc967a5813751414944
Ricky.You <ricky.you@bjss.com>
2024-04-24 14:08:28 +0100

    hackathon24: update scripts. temporarily comment out some code

--- .github/scripts/get-stats.py
+++ .github/scripts/get-stats.py
@@ -4,12 +4,9 @@ import os
 import json
 from datetime import datetime, timedelta
 
-<<<<<<< HEAD
-=======
 data_folder_path = "data"
 
 
->>>>>>> refs/remotes/origin/main
 def get_most_active_contributors(owner_repo: dict):
     file_name = "most_active_contributors.json"
 
@@ -32,16 +29,11 @@ def get_most_active_contributors(owner_repo: dict):
             response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
             repo_contributors[repo] = response.json()
         except requests.RequestException as e:
-<<<<<<< HEAD
-            return "Error sending HTTP request:" + str(e)
-    return repo_contributors
-=======
             print("Error sending HTTP request:", e)
 
     # Write the JSON string to the file
-    with open(os.path.join(data_folder_path, file_name), "w") as json_file:
+    with open(os.path.join(data_folder_path, file_name), "w+") as json_file:
         json_file.write(json.dumps(repo_contributors))
->>>>>>> refs/remotes/origin/main
 
 
 def get_most_active_repos(owner_repo: dict):
@@ -89,13 +81,9 @@ def get_most_active_repos(owner_repo: dict):
 
         repo_active_score[repo] = open_pr_number + commit_number_past_month
 
-<<<<<<< HEAD
-    return repo_active_score
-=======
     # Write the JSON string to the file
-    with open(os.path.join(data_folder_path, file_name), "w") as json_file:
+    with open(os.path.join(data_folder_path, file_name), "w+") as json_file:
         json_file.write(json.dumps(repo_active_score))
->>>>>>> refs/remotes/origin/main
 
 
 owners = sys.argv[1].split(",")
@@ -106,40 +94,31 @@ owner_repo = dict()
 for i in range(len(owners)):
     owner_repo[owners[i]] = repos[i]
 
-<<<<<<< HEAD
-contributors = get_most_active_contributors(owner_repo)
-active_repos = get_most_active_repos(owner_repo)
+# <<<<<<< HEAD
+# contributors = get_most_active_contributors(owner_repo)
+# active_repos = get_most_active_repos(owner_repo)
+#
+# # Generate the top contributors list as a Markdown string
+# top_contributors_md = "\n".join(
+#     f"1. [{contributor['login']}](https://github.com/{contributor['login']}) - {contributor['contributions']} commits"
+#     for contributor in top_contributors
+# )
+#
+# # Read the existing README
+# with open('README.md', 'r') as f:
+#     readme = f.read()
+#
+# # Replace the Top Contributors section
+# updated_readme = re.sub(
+#     r'## Top Contributors\n\n([^#]+)',
+#     f'## Top Contributors\n\n{top_contributors_md}\n',
+#     readme
+# )
+#
+# # Write the updated README
+# with open('README.md', 'w') as f:
+#     f.write(updated_readme)
 
-# Generate the top contributors list as a Markdown string
-top_contributors_md = "\n".join(
-    f"1. [{contributor['login']}](https://github.com/{contributor['login']}) - {contributor['contributions']} commits"
-    for contributor in top_contributors
-)
-
-# Read the existing README
-with open('README.md', 'r') as f:
-    readme = f.read()
-
-# Replace the Top Contributors section
-updated_readme = re.sub(
-    r'## Top Contributors\n\n([^#]+)',
-    f'## Top Contributors\n\n{top_contributors_md}\n',
-    readme
-)
-
-# Write the updated README
-with open('README.md', 'w') as f:
-    f.write(updated_readme)
-=======
 get_most_active_contributors(owner_repo)
 get_most_active_repos(owner_repo)
 
-# output = get_most_active_contributors(owner_repo)
-# output1 = get_most_active_repos(owner_repo)
-
-# output = "Some output"  # Replace "Some output" with the actual output you want to write
-#
-# with open('README.md', 'w') as f:
-#     f.write(output)
-#     f.write(output1)
->>>>>>> refs/remotes/origin/main
